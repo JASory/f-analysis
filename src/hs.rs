58e85ca3b480911a3eab2da8af54081c349ee09b
JASory <jasory@rust-cas.org>
2023-11-25 14:07:41 -0700

    Delete src directory

--- src/hs.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) mod hashsearch;
-pub(crate) mod basesearch;
--- src/math.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod fermat;
-pub(crate) mod ntfunc;
-pub(crate) mod rand;
-pub(crate) mod sieve;
-
-pub(crate) use sieve::plist;
-
-pub use fermat::Pseudoprime;
